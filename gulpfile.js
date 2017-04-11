f5c4f997bd00eaef9e04722723ca75b3cbe95b79
ququzone <xueping.yang@gmail.com>
2017-04-11 16:37:06 +0800

    initial project

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var DEST = 'static/';
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'ui/js/helpers/*.js',
+        'ui/js/*.js',
+      ])
+      .pipe(concat('custom.js'))
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest(DEST+'/js'));
+});
+
+var compileSASS = function (filename, options) {
+  return sass('ui/scss/*.scss', options)
+        .pipe(autoprefixer('last 2 versions', '> 5%'))
+        .pipe(concat(filename))
+        .pipe(gulp.dest(DEST+'/css'));
+};
+
+gulp.task('sass', function() {
+    return compileSASS('custom.css', {});
+});
+
+gulp.task('sass-minify', function() {
+    return compileSASS('custom.min.css', {style: 'compressed'});
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'sass', 'sass-minify']);
