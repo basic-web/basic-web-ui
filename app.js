d85174277ba183a963666282354b4e75d046ceb3
ququzone <xueping.yang@gmail.com>
2017-04-13 17:04:06 +0800

    change to arrow function style

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/', function(req, res) {
+app.use('/', (req, res) => {
     res.send('Hello basic web ui.')
 });
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
