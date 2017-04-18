1f21293809e4b9e2e9e3b94f4bd71f75c5840321
ququzone <xueping.yang@gmail.com>
2017-04-18 15:04:08 +0800

    complete session check middleware

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const config = {
     app: {
         name: 'Basic Web UI!'
--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
+'use strict';
+
 const users = require('./modules/users');
 
 module.exports = app => {
+    app.use((req, res, next) => {
+        if (['/login', '/register'].indexOf(req.path) === -1 && !req.session.userID) {
+            return res.redirect('/login');
+        }
+        next();
+    });
     users(app);
-}
+};
