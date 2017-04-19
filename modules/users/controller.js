85492e437c180c7c8a8f441ff1c7d79116874123
ququzone <xueping.yang@gmail.com>
2017-04-19 21:01:09 +0800

    complete register logic

--- modules/users/controller.js
+++ modules/users/controller.js
@@ -12,7 +12,29 @@ exports.login = (req, res) => {
         }
         service.login(req.body).then(user => {
             req.session.userID = user.id;
-            res.end();
+            res.json({});
+        }).catch(err => {
+            if (err.name === 'StatusCodeError') {
+                res.status(err.statusCode).json(err.error);
+            } else {
+                res.status(500).json({ message: err.message });
+            }
+        });
+    });
+};
+
+exports.register = (req, res) => {
+    req.checkBody('nickname', '昵称过长').isLength({ min: 1, max: 20 });
+    req.checkBody('phone', '电话号码错误').isMobilePhone('zh-CN');
+    req.checkBody('password', '密码不能为空').notEmpty();
+    req.getValidationResult().then(result => {
+        if (!result.isEmpty()) {
+            res.status(400).json({ error: result.array() });
+            return;
+        }
+        service.register(req.body).then(user => {
+            req.session.userID = user.id;
+            res.json({});
         }).catch(err => {
             if (err.name === 'StatusCodeError') {
                 res.status(err.statusCode).json(err.error);
